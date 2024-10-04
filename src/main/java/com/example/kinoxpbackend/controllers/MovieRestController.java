e132b000fa25f17ebb4b0fd826145462465c846f
sgtasswipe <otro0001@stud.kea.dk>
2024-10-04 13:22:17 +0200

    added MovieRestController & MovieRepository

---
+++ src/main/java/com/example/kinoxpbackend/controllers/MovieRestController.java
@@ -0,0 +1,61 @@
+package com.example.kinoxpbackend.controllers;
+
+import com.example.kinoxpbackend.models.Movie;
+import com.example.kinoxpbackend.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieRestController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> findAll() {
+        List<Movie> movies = movieRepository.findAll();
+        return ResponseEntity.ok().body(movies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> findById(@PathVariable int id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/movie")
+    public Movie createMovie (@RequestBody Movie movie ) {
+        return movieRepository.save(movie);
+    }
+
+     @PutMapping("/movie/{id}")
+     public ResponseEntity<Movie> updateMovie(@PathVariable int id, @RequestBody Movie movie) {
+        Optional<Movie> optionalMovie = movieRepository.findById(id);
+        if(optionalMovie.isPresent()) {
+            movieRepository.save(movie);
+            return ResponseEntity.ok(movie);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+     }
+
+    @DeleteMapping("/movie/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable int id) {
+        Optional<Movie> optionalMovie = movieRepository.findById(id);
+
+        if (optionalMovie.isPresent()) {
+            movieRepository.delete(optionalMovie.get());
+            return ResponseEntity.ok("Movie deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Movie not found");
+        }
+    }
+
+}
