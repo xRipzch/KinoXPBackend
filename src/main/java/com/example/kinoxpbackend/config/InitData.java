2abb83381c52a770505f7987ba46f0043a138760
Magne <Magnompower@gmail.com>
2024-10-06 14:05:38 +0200

    Changed movie examples

--- src/main/java/com/example/kinoxpbackend/config/InitData.java
+++ src/main/java/com/example/kinoxpbackend/config/InitData.java
@@ -18,10 +18,10 @@ public class InitData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Movie movie1 = new Movie();
-        movie1.setTitle("Saw X");
-        movie1.setDescription("Lots of gore");
-        movie1.setDurationInMinutes(118);
-        movie1.setReleaseYear(LocalDate.of(2023, 7, 7));
+        movie1.setTitle("Saw CCCCCCCC ASD ASD");
+        movie1.setDescription("Lots of gore Lots of goSAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE AWEAWE AWE AWE AWE AWE AWE AWE AWE AWE AWE AWE AWE AWE ADSD AWEAreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of goreLots of gore");
+        movie1.setDurationInMinutes(1181);
+        movie1.setReleaseYear(LocalDate.of(2025, 7, 7));
         movie1.setImageUrl("https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2023/07/saw-x-poster.jpg?resize=740%2C925&ssl=1");
         movieRepository.save(movie1);
 
